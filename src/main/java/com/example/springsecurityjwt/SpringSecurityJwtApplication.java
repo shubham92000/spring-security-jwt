a802a198b3fc3437915229b006a89c70897fc4a6
shubham92000 <sbhsingh616@gmail.com>
2023-04-18 16:55:46 +0530

    spring security with jwt

---
+++ src/main/java/com/example/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,14 @@
+package com.example.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
